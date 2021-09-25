16c9a305e925d23c300595b0f9c6fbe3049ce7b3
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-09-25 18:00:15 -0300

    Adicionados atributos, método construtor e getters e setters da classe Professores.

--- src/Professores.java
+++ src/Professores.java
@@ -1,3 +1,47 @@
 public class Professores extends Funcionarios {
 
+    private String nivelDeGraducao;
+    private String disciplinaministrada;
+    private int quantidadeDeAlunos;
+    private int quantidadeDeTurmas;
+
+    public Professores(String nome, String cpf, int numeroDeRegistro, String orgaoDeLotacao, double salario, String nivelDeGraducao, String disciplinaministrada, int quantidadeDeAlunos, int quantidadeDeTurmas) {
+        super(nome, cpf, numeroDeRegistro, orgaoDeLotacao, salario);
+        this.nivelDeGraducao = nivelDeGraducao;
+        this.disciplinaministrada = disciplinaministrada;
+        this.quantidadeDeAlunos = quantidadeDeAlunos;
+        this.quantidadeDeTurmas = quantidadeDeTurmas;
+    }
+
+    public String getNivelDeGraducao() {
+        return nivelDeGraducao;
+    }
+
+    public void setNivelDeGraducao(String nivelDeGraducao) {
+        this.nivelDeGraducao = nivelDeGraducao;
+    }
+
+    public String getDisciplinaministrada() {
+        return disciplinaministrada;
+    }
+
+    public void setDisciplinaministrada(String disciplinaministrada) {
+        this.disciplinaministrada = disciplinaministrada;
+    }
+
+    public int getQuantidadeDeAlunos() {
+        return quantidadeDeAlunos;
+    }
+
+    public void setQuantidadeDeAlunos(int quantidadeDeAlunos) {
+        this.quantidadeDeAlunos = quantidadeDeAlunos;
+    }
+
+    public int getQuantidadeDeTurmas() {
+        return quantidadeDeTurmas;
+    }
+
+    public void setQuantidadeDeTurmas(int quantidadeDeTurmas) {
+        this.quantidadeDeTurmas = quantidadeDeTurmas;
+    }
 }
