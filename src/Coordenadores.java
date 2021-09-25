b4c487539d20d4a65d9b89b598042741a6ea8cf8
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-09-25 18:02:46 -0300

    Adicionados atributos, método construtor e getters e setters da classe Coordenadores.

--- src/Coordenadores.java
+++ src/Coordenadores.java
@@ -1,2 +1,11 @@
 public class Coordenadores extends Funcionarios{
+
+    private int professoresSupervisionados;
+
+    public Coordenadores(String nome, String cpf, int numeroDeRegistro, String orgaoDeLotacao, double salario, int professoresSupervisionados) {
+        super(nome, cpf, numeroDeRegistro, orgaoDeLotacao, salario);
+        this.professoresSupervisionados = professoresSupervisionados;
+    }
+
+
 }
